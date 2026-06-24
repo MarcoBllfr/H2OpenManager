443b69aa18a654ac2dcdc9bfeeddfa101c9fb083
marcobllfr <mineeramod@gmail.com>
2026-06-24 18:07:42 +0200

    create a base ui

--- shared/src/commonMain/kotlin/dev/marcobf/h2openmanager/App.kt
+++ shared/src/commonMain/kotlin/dev/marcobf/h2openmanager/App.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dev.marcobf.h2openmanager.presentation.aquarium.AquariumListScreen
 import org.jetbrains.compose.resources.painterResource
 
 import h2openmanager.shared.generated.resources.Res
@@ -23,6 +24,6 @@ import h2openmanager.shared.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-
+        AquariumListScreen()
         }
     }
--- shared/src/commonMain/kotlin/dev/marcobf/h2openmanager/presentation/aquarium/AquariumListScreen.kt
+++ shared/src/commonMain/kotlin/dev/marcobf/h2openmanager/presentation/aquarium/AquariumListScreen.kt
@@ -1,11 +1,52 @@
 package dev.marcobf.h2openmanager.presentation.aquarium
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.koin.compose.koinInject
 
 @Composable
 fun AquariumListScreen(
  viewModel: AquariumListViewModel = koinInject()
 ){
+  val state by viewModel.uiState.collectAsState()
+ when{
+  state.isLoading -> {
+   Box(
+    contentAlignment = Alignment.Center,
+    modifier = Modifier.fillMaxSize()
+   ){
+    CircularProgressIndicator(modifier = Modifier.size(32.dp))
+   }
+  }
+  state.error != null -> {
+   Box(
+    contentAlignment = Alignment.Center,
+    modifier = Modifier.fillMaxSize()
+   ){
+    Text(text = "Error: (${state.error})")
+   }
+  }
 
+  state.aquariums.isEmpty() -> {
+   Box(
+    contentAlignment = Alignment.Center,
+    modifier = Modifier.fillMaxSize()
+   ){
+    Text(text = "No aquariums for now")
+   }
+  }else -> {
+   //creare una LazyColumn per la lista degli acquari
+   Text(text = "List aquariums (${state.aquariums.size})")
+  }
+
+ }
 }
